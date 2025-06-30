e36acd33fb17d9b226204967db5f8e52f4ed7ad2
davidojo1144 <davco1144@gmail.com>
2025-06-30 16:52:27 +0100

    Aora Mobile App, Used React Native, NativeWind/Tailwind Css, Expo, Animatable, Appwrite

--- Aora/app/_layout.jsx
+++ Aora/app/_layout.jsx
@@ -3,6 +3,8 @@ import React, { useEffect } from 'react'
 import { SplashScreen, Stack } from 'expo-router'
 import { useFonts } from 'expo-font'
 
+SplashScreen.preventAutoHideAsync();
+
 const RootLayout = () => {
   const [fontsLoaded, error] = useFonts({
   "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
@@ -18,14 +20,16 @@ const RootLayout = () => {
 
   useEffect(()=> {
     if(error) throw error
-
     if(fontsLoaded) SplashScreen.hideAsync()
   },[fontsLoaded, error])
+
+  if(!fontsLoaded && !error) return null;
 
 
   return (
     <Stack>
         <Stack.Screen name='index' options={{headerShown: false}} />
+        <Stack.Screen name='profile' options={{headerShown: false}} />
     </Stack>
   )
 }
--- Aora/app/index.jsx
+++ Aora/app/index.jsx
@@ -6,7 +6,7 @@ import {Text, View } from "react-native";
 export default function App () {
     return (
         <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-3xl text-blue-500">Aora!</Text>
+            <Text className="text-3xl font-pblack">Aora!</Text>
             <Link href= "/profile">Go to profile</Link>
         </View>
     )
